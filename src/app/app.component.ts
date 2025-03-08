301bb33f26fb1053198890ce9d00ec873792a058
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-08 12:02:41 +0000

    create and add hero section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from "./components/hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
