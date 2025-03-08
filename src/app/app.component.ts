bda7b5375602832f2695348bfd09c915b9ad7345
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-08 12:13:58 +0000

    create and add about page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from "./components/hero/hero.component";
+import { AboutComponent } from "./components/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, HeroComponent],
+  imports: [CommonModule, HeaderComponent, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
