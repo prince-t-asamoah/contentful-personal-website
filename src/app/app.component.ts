d50fe6544a7555cd381108ec2725af0cfdd13ad8
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-08 12:34:01 +0000

    create and add contacts section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { HeroComponent } from "./components/hero/hero.component";
-import { AboutComponent } from "./components/about/about.component";
-import { ProjectsComponent } from "./components/projects/projects.component";
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, HeroComponent, AboutComponent, ProjectsComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
